c595751a8529e62b052a70926c30652e73bf5b13
Ravshan-web <savurboyevr@gmail.com>
2022-12-09 20:03:53 +0500

    IF ELSE practices

---
+++ main.js
@@ -0,0 +1,177 @@
+// Misol - 1. 2
+// let a=+prompt('a son = ');
+// if(a>0){
+//     console.log('Musbat')
+// }else if(a==0){
+//     console.log('Nol')
+// }else{
+//     console.log('Manfiy');
+// }
+
+
+// Misol - 3
+// let a=+prompt('a son =');
+// if(a%2==0){
+//     console.log('Juft son')
+// }else if(a<0){
+//     console.log("Siz manfiy son kiritdingiz");
+// }else{
+//     console.log('Toq son');
+// }
+
+// Misol - 4
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// if(a>0 && b>0){
+//     console.log('Rostan ham ikkala son musbat')
+// }else{
+//     console.log('Siz son kiritmadingiz yoki orasida bittasi manfiy bo`lishi mumkin');
+// }
+
+
+//Misol - 5
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// let c =+prompt('c son =');
+
+// if(a>b && b>c){
+//     console.log('Rostlik isbotlandi'+" "+a+" "+b+' dan'+' katta ,'+" "+b+" "+c+' dan'+' katta');
+// }else{
+//     console.log('False');
+// }
+
+//  Misol - 6
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// let c =+prompt('c son =');
+
+// if(a>b && b<c){
+//     console.log(
+//        b + ' son a va c sonlar orasida haqiqatan ham yotadi!'
+//     )
+// }else{
+//     console.log('Rostlik bajarilmadi!');
+// }
+
+
+// Misol - 7
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// if(a%2==1 && b%2==1){
+//     console.log('Sonlar haqiqatan ham toq')
+// }else{
+//     console.log('Siz son kiritmadingiz, yoki sonlardan biri juft bo`lishi mumkin')
+// }
+
+// Misol - 8
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// let c =+prompt('c son =');
+// let d =+prompt('d son =');
+
+// if(a>0 && b>0 && c>0 && d>0){
+//     console.log('sonlar haqiqatanam musbat');
+// }else{
+//     console.log('Manfiy son bo`lishi mumkin');
+// }
+
+
+// Misol - 9
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// let c =+prompt('c son =');
+
+// if(a>0 && b>0 && c>0){
+//     console.log("Hammasi musbat");
+// }else if(a>0 && b>0 && c<0){
+//     console.log("Kiritilgan sonlardan " +(a) +" va " + (b) + " musbat" );
+// }else if(a>0 && b<0 && c>0){
+//     console.log("Kiritilgan sonlardan " +(a) +" va " + (c) + " musbat" );
+// }else if(a<0 && b>0 && c>0){
+//     console.log("Kiritilgan sonlardan " + (b) +" va " + (c) + " musbat" );
+// }else{
+//     console.log("Kiritilgan 3 ta sonlardan kamida ikkitasi musbat bo`lishi kerak");
+// }
+
+
+//Misol - 10
+// let a =+prompt('son kiriting');
+// if(a==1){
+//     console.log('Dushanba');
+// }else if(a==2){
+//     console.log('Seshanba');
+// }else if(a==3){
+//     console.log('Chorshanba');
+// }else if(a==4){
+//     console.log('Payshanba');
+// }else if(a==5){
+//     console.log('Juma');
+// }else if(a==6){
+//     console.log('Shanba');
+// }else if(a==7){
+//     console.log('Yakshanba');
+// }else{
+//     console.log("Bunaqa kun yoq bo`ladi");
+// } 
+
+// Misol - 11
+// let a =+prompt('son kiriting');
+// if(a==90 || a==91){
+//     console.log('Beeline');
+// }else if(a==93 || a==94){
+//     console.log('Ucell');
+// }else if(a==88){
+//     console.log('MobiUZ');
+// }else if(a==33){
+//     console.log('Humans');
+// }else if(a==99){
+//     console.log('Uzmobile');
+// }else{
+//     console.log("Bunaqa operator yo`q");
+// }
+
+// Misol - 12
+// let a =+prompt('Son kiriting!')
+// if(a>0){
+//     console.log(a+1);
+// }else if(a==0){
+//      console.log(a);
+// }else{
+//     console.log(a-1)
+// };
+
+
+// Misol - 13
+// let a =+prompt('Son kiriting!')
+// if(a>0){
+//     console.log(a+3);
+// }else if(a==0){
+//      console.log(a);
+// }else{
+//     console.log(a-2)
+// };
+
+
+// Misol - 14 - 15 - 16
+// let a =+prompt('a son =');
+// let b =+prompt('b son =');
+// let c =+prompt('c son =');
+// console.log(Math.max(a ,b, c) + ' eng kattasi')
+// console.log(Math.min(a, b, c) + ' eng kichigi')
+
+
+//Misol - 19
+// let a=+prompt('Son kiriting, tahlil qilaman!');
+// if(a>0 && a%2==0){
+//     console.log('Musbat va juft');
+// }else if(a<0 && a%2==0){
+//     console.log('Manfiy va juft');
+// }else if(a>0 && a%2==1){
+//     console.log('Musbat va toq');
+// }else{
+//     console.log('Musbat va juft');
+// }
+
+// Misol - 20
+let a=+prompt('Tug`ilgan yilingizni kiriting!');
+console.log('Sizning yoshingiz ' + (2022-a) + ' da');
